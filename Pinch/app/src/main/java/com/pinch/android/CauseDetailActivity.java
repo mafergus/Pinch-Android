16255e2b52244db43bf41128d7f66e7ad1850ba6
Matt <matthew.n.ferguson@gmail.com>
2016-06-17 00:27:05 -0700

    Cause detail page with tab placeholder

--- Pinch/app/src/main/java/com/pinch/android/CauseDetailActivity.java
+++ Pinch/app/src/main/java/com/pinch/android/CauseDetailActivity.java
@@ -3,7 +3,11 @@ package com.pinch.android;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -28,6 +32,10 @@ public class CauseDetailActivity extends AppCompatActivity {
 
     private Button donateButton;
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private CausePagerAdapter causePagerAdapter;
+
     private Cause cause;
 
     @Override
@@ -43,6 +51,12 @@ public class CauseDetailActivity extends AppCompatActivity {
         this.missionsCount = (TextView)findViewById(R.id.missions);
         this.moneyRaised = (TextView)findViewById(R.id.raised);
         this.donateButton = (Button)findViewById(R.id.donateButton);
+
+        tabLayout = (TabLayout)findViewById(R.id.tabs);
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+        causePagerAdapter = new CausePagerAdapter();
+        viewPager.setAdapter(causePagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
         Intent intent = getIntent();
         if (intent != null && intent.getExtras() != null) {
@@ -61,5 +75,20 @@ public class CauseDetailActivity extends AppCompatActivity {
         this.daysRemaining.setText("6");
         this.missionsCount.setText("" + 1);
         this.moneyRaised.setText("" + cause.progressAmount);
+    }
+
+    public static class CausePagerAdapter extends PagerAdapter {
+
+//        private List<String> titles = new ArrayList<String>();
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return false;
+        }
     }
 }
